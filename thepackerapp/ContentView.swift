493fa48b9f9ca8ffb6b4ed5c65a906deff370670
Matt Dyor <matt@dyor.com>
2024-06-20 19:10:58 -0700

    Initial Commit

---
+++ thepackerapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  thepackerapp
+//
+//  Created by Matt Dyor on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!s")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ thepackerapp/thepackerappApp.swift
@@ -0,0 +1,17 @@
+//
+//  thepackerappApp.swift
+//  thepackerapp
+//
+//  Created by Matt Dyor on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct thepackerappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
