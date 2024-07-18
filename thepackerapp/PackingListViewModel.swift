05316b6229fc663ca508db1de54c23285cfc8e44
Matt Dyor <matt@dyor.com>
2024-07-18 19:21:50 +0100

    Uploading a broken version for Dustin

--- thepackerapp/PackingListViewModel.swift
+++ thepackerapp/PackingListViewModel.swift
@@ -7,29 +7,43 @@
 
 import Combine
 import Foundation
+import shared
+
+
+
 
 class PackingListViewModel: ObservableObject {
     @Published var items: [PackingItem] = []
-
     func fetchItems() {
+        
         guard let url = URL(string: "https://dyor.github.io/thepackerapp/default_packing_list.json?t=2") else {
             print("Invalid URL")
             return
         }
 
         URLSession.shared.dataTask(with: url) { data, response, error in
-                    if let data = data, error == nil {
-                        do {
-                            let decodedResponse = try JSONDecoder().decode(Feed.self, from: data)
-                            DispatchQueue.main.async {
-                                self.items = decodedResponse.feed
-                            }
-                        } catch {
-                            print("Decoding failed: \(error)")
+
+
+                
+//            itemDao.getAllItems().collect { itemList ->
+//                            _items.value = itemList
+//                            Log.d("PackingListViewModel4", itemList.firstOrNull().toString())
+//                            Log.d("PackingListViewModel3", "Fetched ${itemList.size} items ")
+//                        }
+            //if (_items.value.isEmpty()) {
+                if let data = data, error == nil {
+                    do {
+                        let decodedResponse = try JSONDecoder().decode(Feed.self, from: data)
+                        DispatchQueue.main.async {
+                            self.items = decodedResponse.feed
                         }
-                    } else {
-                        print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+                    } catch {
+                        print("Decoding failed: \(error)")
                     }
+                } else {
+                    print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+                }
+            //}
                 }.resume()
     }
 }
